b7a21b163e3aa10063996a7b690f8bc3ca1a9a64
taylorgehrts <taylorgehrts@gmail.com>
2023-07-10 18:00:28 -0700

    set up requires, and created prompt array with questions

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-//Code will go here//
+//Imports
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+
+//prompt questions
+const questions = [
+
+    {
+        type: "input",
+        name: "Text",
+        message: "Please select up to three letters to feature in your logo."
+
+    },
+
+    {
+        type: "input",
+        name: "TextColor",
+        message: "Please enter a color keyword (OR a hexadecimal number) for the text."
+    },
+
+    {
+        type: "list",
+        name: "Shape",
+        message: "Please choose a shape for your logo.",
+        choices: ["Circle", "Triangle", "Square"]
+
+    },
+
+    {
+        type: "input",
+        name: "Color",
+        message: "Please enter a color keyword (OR a hexadecimal number) for the shape."
+    },
+
+]

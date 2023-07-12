3fd8b9c1a2758285212ecac0415aacb3ceaf9bbb
taylorgehrts <taylorgehrts@gmail.com>
2023-07-12 12:41:22 -0700

    finished the tests, modified Json file to run test, minor tweeks to prompts, commented, changed file structure to recomended

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Taylor-s_SVG_Generator
-Simple node based logo generator
+# Taylor-s_SVG_Generator [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+- This is a simple program based on node and inquirer that will create a list of prompts in the terminal that a user can answer to generate a simple logo in a SVG file
+- The program uses a file system to write the file
+- Inquirer is used to generate the prompts with questions located in an array
+- The program is invoked with the help of the node runtime enviroment
+- There is also a suite of test that confirm that the SVG was generated properly, this is using the jest package.
+- I had to go back a few times with this project as I didn't read the full instructions until I had the program running.  I then refactored the code to inculde the use of classes and created the testing.
+
+## Installation
+Right now you would need to clone the repository and then navigate to the directory and open the terminal. From there you will need to run npm install to install any dependencies and then run the program
+
+## Usage
+- Simply run the program with the command node index.js in the terminal and then answer the questions, when you are through it will generate the SVG file.
+- To view the logo open the file in the browser
+- [Walkthrough Video](https://drive.google.com/file/d/12BleJ_CEeDh6C69ZzQhjTM6krKM3oK9L/view)
+
+## License
+This project is licensed under the MIT license. Click [License](https://opensource.org/licenses/MIT) for more information.
+
+## Contributing
+Taylor Gehrts
+
+## Tests
+The test here are pretty simple and just test whether the shape will be drawn correctly.  To run the test (after you have installed all dependencies) simply run npm test from the command line while in the appropriate directory.
+
+## Questions
+If you have any questions, you can reach out to me via GitHub or E-mail:
+
+- GitHub: [taylorgehrts](https://github.com/taylorgehrts)
+- Email: [taylorgehrts@gmail.com](mailto:taylorgehrts@gmail.com)
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Imports
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateShape = require("./shapes");
+const generateShape = require("./lib/shapes");
 
 // Prompt questions
 const questions = [
@@ -10,13 +10,16 @@ const questions = [
         name: "text",
         message: "Please enter up to three characters for your logo:",
         validate: function (input) {
-            return input.length <= 3 ? true : "Please enter up to three characters.";
+            return input.length <= 3 ? true : "You are using too many characters please delete and enter ONLY up to three characters.";
         }
     },
     {
         type: "input",
         name: "textColor",
-        message: "Please enter a color keyword (OR a hexadecimal number) for the text:"
+        message: "Please enter a color keyword (OR a hexadecimal number) for the text:",
+        validate: function (input) {
+            return input.length >= 1 ? true : "Please enter a valid color key word or hex value";
+        }
     },
     {
         type: "list",
@@ -27,10 +30,13 @@ const questions = [
     {
         type: "input",
         name: "shapeColor",
-        message: "Please enter a color keyword (OR a hexadecimal number) for the shape:"
+        message: "Please enter a color keyword (OR a hexadecimal number) for the shape:",
+        validate: function (input) {
+            return input.length >= 1 ? true : "Please enter a valid color key word or hex value";
     }
+}
 ];
-
+// Generate the SVG based on answers, also add some CSS for font info
 function generateSVGFile(answers) {
     const { text, textColor, shape, shapeColor } = answers;
     const shapeSVG = generateShape(shape, shapeColor);
@@ -41,7 +47,7 @@ function generateSVGFile(answers) {
         font-family: Arial, sans-serif;
       }
     `;
-
+// SVG styling and layout, using the w3 namespace
     const logoContent = `
       <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
         <style>${cssContent}</style>
@@ -49,7 +55,7 @@ function generateSVGFile(answers) {
         <text x="150" y="100" fill="${textColor}" text-anchor="middle">${text.toUpperCase()}</text>
       </svg>
     `;
-
+//function to write the file
     fs.writeFile("logo.svg", logoContent, err => {
         if (err) {
             console.error("Error writing logo.svg:", err);
@@ -62,7 +68,7 @@ function generateSVGFile(answers) {
     });
 }
 
-
+// main function that calls everthing else and runs the program
 function run() {
     inquirer.prompt(questions)
         .then(answers => {
@@ -72,5 +78,5 @@ function run() {
             console.error("An error occurred:", error);
         });
 }
-
+//calls the run function
 run();

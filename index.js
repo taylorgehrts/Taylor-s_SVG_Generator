c722fe3290bcc92f5956f4bdb53820322edf96fc
taylorgehrts <taylorgehrts@gmail.com>
2023-07-11 19:13:52 -0700

    got everything working and writting to a file

--- index.js
+++ index.js
@@ -1,50 +1,76 @@
-//Imports
+// Imports
 const inquirer = require("inquirer");
 const fs = require('fs');
-const shapes = require("./shapes");
-console.log(shapes);
+const generateShape = require("./shapes");
 
-
-//prompt questions
+// Prompt questions
 const questions = [
-
     {
         type: "input",
-        name: "Text",
-        message: "Please select up to three letters to feature in your logo."
-
+        name: "text",
+        message: "Please enter up to three characters for your logo:",
+        validate: function (input) {
+            return input.length <= 3 ? true : "Please enter up to three characters.";
+        }
     },
-
     {
         type: "input",
-        name: "TextColor",
-        message: "Please enter a color keyword (OR a hexadecimal number) for the text."
+        name: "textColor",
+        message: "Please enter a color keyword (OR a hexadecimal number) for the text:"
     },
-
     {
         type: "list",
-        name: "Shape",
-        message: "Please choose a shape for your logo.",
+        name: "shape",
+        message: "Please choose a shape for your logo:",
         choices: ["Circle", "Triangle", "Square"]
-
     },
-
     {
         type: "input",
-        name: "Color",
-        message: "Please enter a color keyword (OR a hexadecimal number) for the shape."
-    },
+        name: "shapeColor",
+        message: "Please enter a color keyword (OR a hexadecimal number) for the shape:"
+    }
+];
 
-]
+function generateSVGFile(answers) {
+    const { text, textColor, shape, shapeColor } = answers;
+    const shapeSVG = generateShape(shape, shapeColor);
+    const cssContent = `
+      text {
+        font-size: 30px;
+        font-weight: bold;
+        font-family: Arial, sans-serif;
+      }
+    `;
 
-function run() {
-    inquirer
-    .prompt(questions)
-    // .then(answers) => {
-    //     const logoContent = Shapes(answers);
-    //     writeToFile("logo.svg", logoContent);
-    // }
+    const logoContent = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <style>${cssContent}</style>
+        ${shapeSVG}
+        <text x="150" y="100" fill="${textColor}" text-anchor="middle">${text.toUpperCase()}</text>
+      </svg>
+    `;
 
+    fs.writeFile("logo.svg", logoContent, err => {
+        if (err) {
+            console.error("Error writing logo.svg:", err);
+            return;
+        }
+        console.log("Generated logo.svg");
+        
+
+        
+    });
 }
 
-run()
+
+function run() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            generateSVGFile(answers);
+        })
+        .catch(error => {
+            console.error("An error occurred:", error);
+        });
+}
+
+run();
--- shapes.js
+++ shapes.js
@@ -1,7 +1,19 @@
-const shapes = {
+const generateShape = (shape, shapeColor) => {
+  const shapes = {
     circle: `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`,
     triangle: `<polygon points="150,50 100,150 200,150" fill="${shapeColor}" />`,
     square: `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`,
   };
-  
-  module.exports = shapes;
+
+  if (shape === "Circle") {
+    return shapes.circle;
+  } else if (shape === "Triangle") {
+    return shapes.triangle;
+  } else if (shape === "Square") {
+    return shapes.square;
+  } else {
+    throw new Error(`Invalid shape: ${shape}`);
+  }
+};
+
+module.exports = generateShape;

a148d88104f7c8c61a0d0b8a383746afbfcc9860
taylorgehrts <taylorgehrts@gmail.com>
2023-07-11 19:24:10 -0700

    ended up reading further down and relized i need to rewrite the shapes.jsgit add -A now using classes, i rweally hope this is the right idea

--- shapes.js
+++ shapes.js
@@ -1,19 +1,61 @@
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+
+  getShapeColor() {
+    return this.color;
+  }
+
+  draw() {
+    throw new Error("draw() method must be implemented in the child class.");
+  }
+}
+
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+
+  draw() {
+    return `<circle cx="150" cy="100" r="50" fill="${this.getShapeColor()}" />`;
+  }
+}
+
+class Triangle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+
+  draw() {
+    return `<polygon points="150,50 100,150 200,150" fill="${this.getShapeColor()}" />`;
+  }
+}
+
+class Square extends Shape {
+  constructor(color) {
+    super(color);
+  }
+
+  draw() {
+    return `<rect x="100" y="50" width="100" height="100" fill="${this.getShapeColor()}" />`;
+  }
+}
+
 const generateShape = (shape, shapeColor) => {
-  const shapes = {
-    circle: `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`,
-    triangle: `<polygon points="150,50 100,150 200,150" fill="${shapeColor}" />`,
-    square: `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`,
-  };
+  let shapeObj;
 
   if (shape === "Circle") {
-    return shapes.circle;
+    shapeObj = new Circle(shapeColor);
   } else if (shape === "Triangle") {
-    return shapes.triangle;
+    shapeObj = new Triangle(shapeColor);
   } else if (shape === "Square") {
-    return shapes.square;
+    shapeObj = new Square(shapeColor);
   } else {
     throw new Error(`Invalid shape: ${shape}`);
   }
+
+  return shapeObj.draw();
 };
 
 module.exports = generateShape;
